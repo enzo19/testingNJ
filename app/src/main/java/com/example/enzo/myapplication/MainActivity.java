b986f8aff3a9a189d63e4c84c215de32fe330f02
Enzo <enzo@etbox.org>
2015-09-28 14:44:59 +0800

    add something

--- app/src/main/java/com/example/enzo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/enzo/myapplication/MainActivity.java
@@ -6,8 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 import com.loopj.android.http.*;
 import org.json.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import cz.msebera.android.httpclient.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     String inputJob=null;
     EditText name,job;
     Button saveData,loadData;
-
+    ArrayList<HashMap<String, String>>name_List;
+    ListView listlist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         job=(EditText)findViewById(R.id.jobTxt);
         saveData=(Button)findViewById(R.id.saveBtn);
         loadData=(Button)findViewById(R.id.listBtn);
-
+        listlist=(ListView)findViewById(R.id.listView);
+        name_List = new ArrayList<HashMap<String, String>>();
 
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("status", "" + statusCode);
                     Log.d("message", message);
                 } catch (JSONException e) {
-                    Log.d("err:", "" + e); //error in the above string(in this case,yes)!
+                    Log.d("err:", "" + e); //error
                 }
             }
 
@@ -70,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getApi(){
+        listlist.setAdapter(null);
+        name_List.clear();
         //RequestParams params = new RequestParams(); //use if u need query with key and value
         //params.put("name", inputName);
         //params.put("job", inputJob);
@@ -77,9 +88,30 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                
+
                     Log.d("status", "" + statusCode);
-                    Log.d("result: ", response.toString());
+                    //Log.d("result: ", response.toString());
+                try{
+                JSONArray nameList=response.getJSONArray("lists");
+                    for (int i = 0; i < nameList.length(); i++) {
+                        JSONObject abc = nameList.getJSONObject(i);
+                        String theName=abc.getString("name");
+                        String theJob=abc.getString("job");
+
+                        // create new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        // adding to hashmap
+                        map.put("name", theName);
+                        map.put("job", theJob);
+                        name_List.add(map);
+                    }
+                    SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, name_List, R.layout.layout, new String[] {"name","job"},
+                            new int[] {R.id.item1,R.id.item2});
+                    listlist.setAdapter(adapter);
+                }
+                catch(JSONException e){
+                    Log.d("err:", "" + e); //error
+                }
 
             }
 
